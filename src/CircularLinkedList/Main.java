aec04253d845b44e2095ee5f856d33c033156ce9
basmah <bsmthashd@gmail.com>
2026-01-09 05:32:38 -0800

    lab4_CircularyLinkedList = pt1

--- src/CircularLinkedList/Main.java
+++ src/CircularLinkedList/Main.java
@@ -1,4 +1,28 @@
 package CircularLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        CircularyLinkedList<String> list=new  CircularyLinkedList<>();
+        {
+            list.addlast("Alaa");
+            list.addlast("Shahd");
+            list.addlast("Maryam");
+            list.addFirst("Ghofran");
+            list.addlast("Basma");
+            list.addFirst("Amat");
+            list.addlast("Jehad");
+            list.addlast("Tasneem");
+            list.removeFirst();
+            list.removeLast();
+            list.removeFirst();
+            list.display();
+
+
+
+
+
+
+        }
+    }
+
 }
